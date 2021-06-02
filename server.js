557fa7be3ecca78c67ee2cb70b6f1266a96d15bb
6h15m <s2019w35@e-mirim.hs.kr>
2021-06-02 09:52:47 +0900

    .env commit

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-// require('dotenv/config');
+require('dotenv/config');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 db.on('error', function (err){
     console.log(err);
